c68b7633e819643628ef2ef6cde3a9e4840faa56
hyyy3 <25hanayuki@gmail.com>
2025-04-29 10:32:01 +0900

    package compact

--- src/main/java/ToiletGo/Application.java
+++ src/main/java/ToiletGo/Application.java
@@ -1,4 +1,4 @@
-package com.toiletgo.ToiletGo;
+package ToiletGo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/ToiletGo/ApplicationTests.java
+++ src/test/java/ToiletGo/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.toiletgo.ToiletGo;
+package ToiletGo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
