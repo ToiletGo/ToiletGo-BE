1fb7f8019cb4d473017474703c32db3845e7958b
hyyy3 <25hanayuki@gmail.com>
2025-05-12 17:06:44 +0900

    Stage - init data

--- src/main/java/sql/SQLMapper.java
+++ src/main/java/sql/SQLMapper.java
@@ -25,18 +25,20 @@ public class SQLMapper {
         String password = props.getProperty("jdbc.password");
 
         String fileName = "src/main/resources/toilet.csv";
-        String insertSQL = "INSERT INTO toilet () " +
-                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        String insertSQL = "INSERT INTO toilet (toilet_id, road_address, lot_address, latitude, longitude, building_name, gu_name, tel_no, toilet_type, open_time, toilet_usage, toilet_status, has_handicap_access, facilities, sign_info, location_detail, note) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
         try (
                 BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "UTF-8"));
                 Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
                 PreparedStatement pstmt = conn.prepareStatement(insertSQL)
         ) {
+            conn.setAutoCommit(false);
             String line;
             boolean isFirstLine = true;
 
             while ((line = br.readLine()) != null) {
+                // System.out.println("읽은 줄: " + line); //
                 if (isFirstLine) {
                     isFirstLine = false;
                     continue;
@@ -45,27 +47,38 @@ public class SQLMapper {
                 String[] fields = parseCSVLine(line);
                 if (fields.length < 17) continue;
 
-                pstmt.setString(1, fields[1]);
-                pstmt.setString(2, fields[2]);
-                pstmt.setDouble(3, parseDouble(fields[3]));
-                pstmt.setDouble(4, parseDouble(fields[4]));
-                pstmt.setString(5, fields[5]);
-                pstmt.setString(6, fields[7]);
-                pstmt.setString(7, fields[8]);
-                pstmt.setString(8, fields[9]);
-                pstmt.setString(9, fields[10]);
-                pstmt.setString(10, fields[11]);
-                pstmt.setBoolean(11, !fields[12].trim().isEmpty());
-                pstmt.setString(12, fields[13]);
-                pstmt.setString(13, fields[14]);
-                pstmt.setString(14, fields[16]);
+                // parseDouble(fields[3])
+
+                pstmt.setString(1, fields[0]); // 1 연번 toilet_id integer
+                pstmt.setString(2, fields[1]); // 2 도로명주소 road_address varchar
+                pstmt.setString(3, fields[2]); // 3 지번주소 lot_address varchar
+                pstmt.setDouble(4, parseDouble(fields[3])); // 4 x 좌표 (경도) latitude decimal
+                pstmt.setDouble(5, parseDouble(fields[4])); // 5 y 좌표 (위도) longitude decimal
+                pstmt.setString(6, fields[5]); // 6 건물명 building_name varchar
+                pstmt.setString(7, fields[6]); // 7 구 명칭 gu_name varchar
+                pstmt.setString(8, fields[7]); // 8 전화번호 tel_no varchar
+                pstmt.setString(9, fields[8]); // 9 유형 toilet_type varchar
+                pstmt.setString(10, fields[9]); // 10 개방시간 open_time varchar
+                pstmt.setString(11, fields[10]); // 11 소재지 용도 toilet_usage varchar
+                pstmt.setString(12, fields[11]); // 12 화장실 현황 toilet_status varchar
+                pstmt.setBoolean(13, !fields[12].trim().isEmpty()); // 13 장애인화장실 현황 has_handicap_access boolean
+                pstmt.setString(14, fields[13]); // 14 편의시설 facilities text
+                pstmt.setString(15, fields[14]); // 15 안내표지 sign_info
+                pstmt.setString(16, fields[15]); // 16 소재지 location_detail
+                pstmt.setString(17, fields[16]); // 17 비고 note
+
 
                 pstmt.executeUpdate();
             }
+            conn.commit();
 
             System.out.println("csv data inserted.");
 
+        } catch (SQLException ex) {
+            System.err.println("삽입 실패: " + ex.getMessage());
+            ex.printStackTrace();
         } catch (Exception e) {
+            // conn.rollback();  // 추가
             e.printStackTrace();
         }
     }
