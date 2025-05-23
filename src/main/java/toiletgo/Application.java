5f9bff3da777fd35a56d999cd37bb248cf547839
hyyy3 <25hanayuki@gmail.com>
2025-05-23 21:39:15 +0900

    commit 05-23-5

--- src/main/java/toiletgo/Application.java
+++ src/main/java/toiletgo/Application.java
@@ -52,6 +52,7 @@ public class Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		User user1 = new User("user_id","user", "$2a$12$j8b7ZPi/44Otq2khAgfAQOeqDH.Y3sUIWIGNrk9Tf2j6jj2zU8udO");
+		User user2 = new User("user2","username2", "$2a$12$j8b7ZPi/44Otq2khAgfAQOeqDH.Y3sUIWIGNrk9Tf2j6jj2zU8udO");
 		Toilet toilet1 = new Toilet("강남구 테헤란로 1", new BigDecimal("127.00000001"), new BigDecimal("36.00000001"));
 		Toilet toilet2 = new Toilet("서초구 신반포로 12", new BigDecimal("126.99999929"), new BigDecimal("36.00000001"));
 
@@ -67,16 +68,23 @@ public class Application implements CommandLineRunner {
 		GiftList giftList3 = new GiftList("Moms Touch", "https://www.google.com/imgres?q=%EC%8B%B8%EC%9D%B4%EB%B2%84%EA%B1%B0%20%EA%B8%B0%ED%94%84%ED%8B%B0%EC%BD%98%20%EC%82%AC%EC%A7%84&imgurl=https%3A%2F%2Fshop2.daumcdn.net%2Fthumb%2FR500x500%2F%3Ffname%3Dhttp%253A%252F%252Fshop2.daumcdn.net%252Fshophow%252Fp%252FM29422699655.jpg%253Fut%253D20241025063306&imgrefurl=https%3A%2F%2Fm.shoppinghow.kakao.com%2Fm%2Fsearch%2Fq%2F%25EC%258B%25B8%25EC%259D%25B4%25EB%25B2%2584%25EA%25B1%25B0%2520%25EA%25B8%25B0%25ED%2594%2584%25ED%258B%25B0%25EC%25BD%2598&docid=W8CkUUwd9hxEkM&tbnid=pPSoWb0mnbQtPM&vet=12ahUKEwiC-8vfxLmNAxXooK8BHae_H9EQM3oECBoQAA..i&w=500&h=500&hcb=2&ved=2ahUKEwiC-8vfxLmNAxXooK8BHae_H9EQM3oECBoQAA"
 				, 300, LocalDate.of(2025, 6, 1), false);
 
+		Review review1 = new Review(user2, toilet1, 5, "최고예요!", LocalDateTime.now());
+		Review review2 = new Review(user1, toilet2, 5, "최고예요!", LocalDateTime.now());
+		Report report1 = new Report(user2, review1, null, "사실과 다른 정보", "비데 없음", false, LocalDateTime.now());
+
 
 		Gift gift1 = new Gift(giftList3, user1, false, false);
 
 		userRepository.save(user1);
+		userRepository.save(user2);
 		userRepository.save(new User("admin_id", "admin", "$2a$12$T05/pakINgU7nUagCdInRe8rC6xPK1sHuhxlUuSIQPENfAogqeGpG"));
 
 		toiletRepository.save(toilet1);
 		toiletRepository.save(toilet2);
 
-		reviewRepository.save(new Review(user1, toilet2, 5, "최고예요!", LocalDateTime.now()));
+		reviewRepository.save(review1);
+		reviewRepository.save(review2);
+		reportRepository.save(report1);
 
 		missionListRepository.save(missionList1);
 		missionListRepository.save(missionList2);
@@ -87,6 +95,7 @@ public class Application implements CommandLineRunner {
 		giftListRepository.save(giftList3);
 
 		giftRepository.save(gift1);
+
 
 
 
--- src/main/java/toiletgo/activities/entity/Report.java
+++ src/main/java/toiletgo/activities/entity/Report.java
@@ -8,6 +8,9 @@ import toiletgo.user.entity.User;
 
 import java.time.LocalDateTime;
 
+/**
+ * Report Entity 클래스
+ */
 
 @Data
 @NoArgsConstructor
@@ -20,25 +23,36 @@ public class Report {
     @Column(unique = true, nullable = false, name = "report_id")
     private Long reportId;
 
+    public Report(User user, Review review, Toilet toilet, String reportType, String description, boolean isProcessed, LocalDateTime reportAt ) {
+        this.user = user;
+        this.review = review;
+        this.toilet = toilet;
+        this.reportType = reportType;
+        this.description = description;
+        this.isProcessed = isProcessed;
+        this.reportAt = reportAt;
+    }
+
     @ManyToOne // optional = true
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @ManyToOne // optional = true
-    @JoinColumn(name="review_id", nullable = false)
+    @JoinColumn(name="review_id", nullable = true)
     private Review review;
 
     @ManyToOne // optional = true
-    @JoinColumn(name="toilet_id", nullable = false)
+    @JoinColumn(name="toilet_id", nullable = true)
     private Toilet toilet;
 
+    // 신고 사유
     @Column(name = "report_type", nullable = false)
     private String reportType;
 
     @Column(name = "description", nullable = false)
     private String description;
 
-    @Column(nullable = false, name="is_processed")
+    @Column(name="is_processed")
     private boolean isProcessed;
 
     @Column(nullable = false, name="report_at")
